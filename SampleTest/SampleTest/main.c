76ced341a725ad169c720e418b5ce8a9167a973c
prodr010 <prodr010@ucr.edu>
2018-04-12 15:08:13 -0700

    simple keypad with led bank working

---
+++ SampleTest/SampleTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * SampleTest.c
+ *
+ * Created: 4/11/2018 10:33:31 PM
+ * Author : Paul
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
